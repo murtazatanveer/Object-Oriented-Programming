6d8c33e79c54f1fed197d4f3dd553885cdb14e82
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-03-13 01:24:57 +0500

    Decimal to Binary conversion in java.

---
+++ Java Fundamentals/decimalToBinary.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class decimalToBinary{
+
+	public static void main(String [] str){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter Decimal Number : ");
+		int decimalNum = input.nextInt();
+		String binaryNum = "";
+
+		int digit;
+
+		while(decimalNum>1){
+
+			digit = decimalNum % 2;
+
+			decimalNum /= 2;
+
+			if(digit==1){
+
+				binaryNum = "1" + binaryNum;
+
+				}else{
+
+					binaryNum = "0" + binaryNum;
+
+					}
+
+
+			}
+
+			binaryNum = "1" + binaryNum;
+		    System.out.println("\nAfter converting "+decimalNum+" into Binary Number : "+binaryNum);
+
+
+		}
+	}
