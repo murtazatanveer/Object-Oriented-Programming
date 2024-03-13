6474524b154fec2c33cf70ce88a75e9857d9f65b
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-03-14 00:07:25 +0500

    Number System Conversions.

---
+++ Java Fundamentals/Functions/numberSystemConversions.java
@@ -0,0 +1,73 @@
+package Functions;
+
+public class numberSystemConversions{
+
+    public static void main(String[] args){
+
+System.out.println(octalToBinary(777));
+
+ }
+
+ static String octalToBinary(int octalNum){
+
+     int digit=0;
+
+     String reqBinaryNum="";
+
+     int length=0;
+
+     String binaryNum="";
+
+     while(octalNum>0){
+
+          digit = octalNum % 10;
+         reqBinaryNum = decimaltobinary(digit);
+         length = reqBinaryNum.length();
+
+         if(length<3){
+
+             if(length == 2){reqBinaryNum = 0 + reqBinaryNum;}
+
+             else{reqBinaryNum = "0"+"0"+reqBinaryNum;}}
+
+
+
+         binaryNum  = reqBinaryNum + binaryNum;
+
+         octalNum/=10;}
+
+
+
+         return binaryNum;}
+
+
+     static String decimaltobinary(int decimalNum){
+         int digit;
+
+         String binaryNum="";
+
+                 while(decimalNum>1){
+
+                     digit = decimalNum % 2;
+
+                     decimalNum /= 2;
+
+                     if(digit==1){
+
+                         binaryNum = "1" + binaryNum;
+
+                         }else{
+
+                             binaryNum = "0" + binaryNum;
+
+                             }
+
+
+                     }
+
+            binaryNum = "1" + binaryNum;
+
+            return binaryNum;
+
+         }
+   }
---
+++ Java Fundamentals/Functions/sum.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class sum {
+
+    
+    public static void main(String[] args) {
+
+        System.out.print(add(10, 30));
+        
+
+ }
+ static int add(int n1 , int n2){return n1+n2;}
+    
+}
