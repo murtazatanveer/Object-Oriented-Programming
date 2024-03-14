114ed1cbee6600261ad8179d841dc8ecb22e3ed4
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-03-14 22:39:48 +0500

    Array Reverse

---
+++ Java Fundamentals/Arrays/arrayReverse.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class arrayReverse{
+
+	public static void main(String [] str){
+
+		int [] evenNumbers = {2,4,6,8,10};
+
+		array_reverse(evenNumbers);
+
+		// Reversed array is
+
+		for(int i = 0;i<evenNumbers.length;i++){
+
+			System.out.print(evenNumbers[i]+" ");
+
+			}
+
+		}
+
+		public static int[] array_reverse(int [] numbers){
+
+			int temp = 0;
+
+			for(int i =0;i< (int)(numbers.length/2);i++){
+
+				temp = numbers[i];
+				numbers[i] = numbers[numbers.length - i-1];
+				numbers[numbers.length - i-1] = temp;
+
+				}
+
+				return numbers;
+
+			}
+
+	}
