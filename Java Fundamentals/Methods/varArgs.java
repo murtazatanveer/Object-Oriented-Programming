a47d709d69209e8fd543d0ab8712531d8cbbc7c7
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-03-27 00:53:08 +0500

    Var Args.

---
+++ Java Fundamentals/Methods/varArgs.java
@@ -0,0 +1,26 @@
+package Methods;
+import java.util.Arrays;
+
+public class varArgs{
+
+	static void changeArray(int ...arr){
+
+			int add=5;
+
+		for(int i =0;i<arr.length;i++){
+				arr[i] = add;
+				add += 10;
+				}
+
+		}
+
+	public static void main(String [] str){
+
+		int [] numbers = {10,20,30,40,50,60};
+
+        changeArray(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+		}
+	}
