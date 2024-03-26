c525ee8f3b26b525d814c869ed01b56a9f47c63e
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-03-26 22:55:43 +0500

    for each loop

---
+++ Java Fundamentals/Methods/for_each_loop.java
@@ -0,0 +1,17 @@
+package Methods;
+
+public class for_each_loop{
+
+	public static void main(String [] str){
+
+		int [] numbers = {10,24,60,34,27,12,50,48};
+		
+		for(int x : numbers){
+
+			System.out.println(x);
+
+			}
+
+		}
+
+	}
